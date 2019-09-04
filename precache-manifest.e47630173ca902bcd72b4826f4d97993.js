0a6bc5dd74167f8e1912c31be5353d4f78126b8b
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-09-04 13:56:58 -0400

    Updates

---
+++ precache-manifest.e47630173ca902bcd72b4826f4d97993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f67b41cae7834b36dd3eb5bd95b71b6",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "3920fd2e6a19ac4a90ed",
+    "url": "/Clicky-Game/static/css/main.b2581933.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/Clicky-Game/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "3920fd2e6a19ac4a90ed",
+    "url": "/Clicky-Game/static/js/main.b36c7aa4.chunk.js"
+  },
+  {
+    "revision": "75fa83e2a97bc1c69d72",
+    "url": "/Clicky-Game/static/js/runtime~main.4267543b.js"
+  }
+]);
